0d3ea95059b77bb8b63db8523a0269068d78b0f8
Tim Smith <tim@cozy.co>
2015-05-30 13:33:50 -0700

    Update version library

--- lib/sensu-plugins-opsgenie.rb
+++ lib/sensu-plugins-opsgenie.rb
@@ -1,7 +1,15 @@
+
+require 'sensu-plugins-opsgenie/version'
+
+# Load the defaults
+
 #
-# Set gem version
+# Default class
 #
 module SensuPluginsOpsgenie
-  # Gem version
-  VERSION = '0.0.1.alpha.1'
+  class << self
+  end
+
+  class << self
+  end
 end
