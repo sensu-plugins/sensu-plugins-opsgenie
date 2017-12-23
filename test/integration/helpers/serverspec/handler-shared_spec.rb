2a263489fb78565b435ff7c0b98b1c7b88f5eda8
TJ Saunders <tsaunders@tesla.com>
2017-12-23 10:46:49 -0800

    Now that I have kitchen-docker properly installed, I can address these test errors properly.

--- test/integration/helpers/serverspec/handler-shared_spec.rb
+++ test/integration/helpers/serverspec/handler-shared_spec.rb
@@ -6,22 +6,27 @@ gem_path = '/usr/local/bin'
 handler_name = 'handler-opsgenie.rb'
 handler = "#{gem_path}/#{handler_name}"
 
-# Example JSON input to the OpsGenie handler, from Sensu, via stdin.
+# Example JSON input to the OpsGenie handler, from Sensu, via stdin.  We write
+# it to a file to avoid shell-isms.
+create_alert_file = '/tmp/create-alert.json'
 create_alert = '{"client":{"name":"test01","address":"127.0.0.1","subscriptions":["all"],"timestamp":1326390159},"check":{"name":"some.fake.check.name","issued":1326390169,"output":"CRITICAL: text\n","status":2,"notification":"check failed","command":"/path/to/some/stuff/here -A do_smoke","subscribers":["all"],"interval":60,"handlers":["default","opsgenie"],"history":["0","0","2"],"flapping":false},"occurrences":1,"action":"create"}'
+File.open(create_alert_file, 'w') { |f| f.write(create_alert) }
 
 # Example JSON input to the OpsGenie handler, from Sensu, via stdin.
+resolve_alert_file = '/tmp/resolve-alert.json'
 resolve_alert = '{"client":{"name":"test01","address":"127.0.0.1","subscriptions":["all"],"timestamp":1326390159},"check":{"name":"some.fake.check.name","issued":1326390169,"output":"CRITICAL: text\n","status":2,"notification":"check failed","command":"/path/to/some/stuff/here -A do_smoke","subscribers":["all"],"interval":60,"handlers":["default","opsgenie"],"history":["0","0","2"],"flapping":false},"occurrences":1,"action":"resolve"}'
+File.open(resolve_alert_file, 'w') { |f| f.write(resolve_alert) }
 
 # These tests would require a valid OpsGenie API key and heartbeat name
 # configured in order to succeed.  Thus for now, we limit ourselves to the
 # expected failure cases.
 
 describe 'handler-opsgenie' do
-  describe command("echo '#{create_alert}' | #{handler}") do
+  describe command("#{handler} < #{create_alert_file}") do
     its(:stdout) { should match(/failed to create incident.*not authorized/) }
   end
 
-  describe command("echo '#{resolve_alert}' | #{handler}") do
+  describe command("#{handler} < #{resolve_alert_file}") do
     its(:stdout) { should match(/failed to resolve incident.*not authorized/) }
   end
 end
